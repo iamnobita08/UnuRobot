75ca649d0ad658331fb582404762ec2557e91c3d
iamnobita08 <nobitamusicrobot@gmail.com>
2025-07-19 18:39:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from pyrogram import Client
 
 API_ID = int(os.environ.get("API_ID", 23343216))
 API_HASH = os.environ.get("API_HASH", "1d66f21cd828dc22b80e3750719bd94a")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7998056212:AAF5kSFstAKKYRlf8_u_kXjlrYxZmsJas0Q")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
 # Optional settings
 MIN_PLAYERS = int(os.environ.get("MIN_PLAYERS", 2))
